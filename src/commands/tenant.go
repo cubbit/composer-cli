dc0887f9b81bac2e48ff3a67a3bfc553cd22e9f2
Ginevra <ginevra.valerio@steamschoolemilia.it>
2023-07-03 17:06:12 +0200

    feat(tenant): interface implementation

---
+++ src/commands/tenant.go
@@ -0,0 +1,59 @@
+package commands
+
+import (
+	"github.com/cubbit/cubbit/client/cli/src/actions"
+	"github.com/urfave/cli/v2"
+)
+
+func Tenant() *cli.Command {
+	command := cli.Command{
+		Name:  "tenant",
+		Usage: "execute commands in tenant sections",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "name",
+				Aliases: []string{"n"},
+				Usage:   "Configuration Name",
+			},
+			&cli.StringFlag{
+				Name:        "config",
+				Usage:       "Configuration path for file ./",
+				DefaultText: "./",
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			return nil
+		},
+		Subcommands: []*cli.Command{
+			{
+				Name:  "create",
+				Usage: "create a new tenant",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "name",
+						Aliases:  []string{"n"},
+						Usage:    "The name of the tenant",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:    "description",
+						Aliases: []string{"desc"},
+						Usage:   "A meaningful desciription of the tenant",
+					},
+					&cli.StringFlag{
+						Name:  "image-url",
+						Usage: "The url of an image",
+					},
+					&cli.StringFlag{
+						Name:    "settings",
+						Aliases: []string{"s"},
+						Usage:   "A Json object containing the settings",
+					},
+				},
+				Action: actions.CreateTenant,
+			},
+		},
+	}
+
+	return &command
+}
