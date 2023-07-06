6a60745461b779174d2653b3f6f649102221d20a
Ginevra <ginevra.valerio@steamschoolemilia.it>
2023-07-06 12:17:55 +0200

    feat(tenant): lists all the tenants

--- src/api/response_extractors.go
+++ src/api/response_extractors.go
@@ -97,3 +97,24 @@ func extractGenericIDResponseModel(response *GenericIDResponseModel) request_uti
 		return nil
 	}
 }
+
+func extractTenantListModel(response *TenantList) request_utils.RequestModifier {
+	return func(opt *request_utils.RequestOptions, res *http.Response) error {
+		var err error
+		var body []byte
+
+		if res == nil {
+			return nil
+		}
+
+		if body, err = ioutil.ReadAll(res.Body); err != nil {
+			return err
+		}
+
+		if err = json.Unmarshal(body, &response); err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
