9dc5468ce94c543973d4dce4519aa68ee039fc4e
Ginevra <ginevra.valerio@steamschoolemilia.it>
2023-07-06 14:39:49 +0200

    feat(tenant): gives a list of available tenants with their infomation

--- src/api/response_extractors.go
+++ src/api/response_extractors.go
@@ -118,3 +118,24 @@ func extractTenantListModel(response *TenantList) request_utils.RequestModifier 
 		return nil
 	}
 }
+
+func extractOperatorResponseModel(response *Operator) request_utils.RequestModifier {
+	return func(opt *request_utils.RequestOptions, res *http.Response) error {
+		var err error
+		var body []byte
+
+		if res == nil {
+			return nil
+		}
+
+		if body, err = ioutil.ReadAll(res.Body); err != nil {
+			return err
+		}
+
+		if err = json.Unmarshal(body, &response); err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
