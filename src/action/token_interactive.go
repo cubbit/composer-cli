faa59505c2a143a80d0c3b0b7aa735c7b0870015
Asma <faouzi.joinit@gmail.com>
2023-09-27 17:56:21 +0200

    feat(cli): add access token generation cmd

--- src/action/token_interactive.go
+++ src/action/token_interactive.go
@@ -4,19 +4,18 @@ import (
 	"fmt"
 
 	"github.com/cubbit/cubbit/client/cli/constants"
-	"github.com/cubbit/cubbit/client/cli/src/api"
 	"github.com/cubbit/cubbit/client/cli/src/configuration"
 	"github.com/cubbit/cubbit/client/cli/utils"
 	"github.com/spf13/cobra"
 )
 
-func GenerateAccessToken(cmd *cobra.Command, args []string) error {
+func GenerateAccessTokenInteractive(cmd *cobra.Command) error {
 	var err error
 	var accessToken *string
 	var configPath string
 	var conf *configuration.Config
 
-	if conf, configPath, err = configuration.ReadConfig(cmd); err != nil {
+	if conf, configPath, err = configuration.ReadConfig(cmd, true); err != nil {
 		return fmt.Errorf("%s: %w", constants.ErrorLoadingConfig, err)
 	}
 
@@ -32,21 +31,4 @@ func GenerateAccessToken(cmd *cobra.Command, args []string) error {
 	fmt.Printf("Token: %s\n", *accessToken)
 
 	return nil
-}
-
-func rehydrateTokenConfig(configPath string, conf *configuration.Config) (*string, error) {
-	var accessToken, refreshToken string
-	var err error
-
-	if accessToken, refreshToken, err = api.RefreshOperatorAccessToken(conf.Urls, conf.RefreshToken); err != nil {
-		return nil, fmt.Errorf("%s: %w", constants.ErrorGeneratingToken, err)
-	}
-
-	conf.RefreshToken = refreshToken
-
-	if err = conf.StoreSession(configPath); err != nil {
-		return nil, fmt.Errorf("%s: %w", constants.ErrorStoringSession, err)
-	}
-
-	return &accessToken, nil
 }
